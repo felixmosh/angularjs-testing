7dcc0dd3033722b598cf5674ef662ee786a0f750
Felix Mosheev <felixm@wix.com>
2018-07-23 00:04:16 +0300

    Add mock & spy test

--- src/app/app/app.spec.js
+++ src/app/app/app.spec.js
@@ -1,2 +1,23 @@
+import 'angular-mocks';
+
 describe('app', () => {
+  let component;
+  let googleAnalytics;
+  beforeEach(angular.mock.module('app', {
+    googleAnalytics: {
+      trackLoad: jasmine.createSpy('trackLoad')
+    }
+  }));
+
+  beforeEach(angular.mock.inject(($compile, $rootScope, $httpBackend, _googleAnalytics_) => {
+    $httpBackend.whenGET(/.*/).respond([]);
+    const scope = $rootScope.$new();
+    component = $compile(`<app></app>`)(scope);
+    scope.$apply();
+    googleAnalytics = _googleAnalytics_;
+  }));
+
+  it('should send google analytics', () => {
+    expect(googleAnalytics.trackLoad).toHaveBeenCalledWith('app-loaded');
+  });
 });
