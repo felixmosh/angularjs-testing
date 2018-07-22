c1c45a8d028d4cb0a9197c8d57786425fbd21efd
Felix Mosheev <felixm@wix.com>
2018-07-23 00:15:19 +0300

    No tests

--- e2e/app.component.e2e-spec.js
+++ e2e/app.component.e2e-spec.js
@@ -1,42 +1,10 @@
-function elementByDataHook(hook) {
-  return $(`[data-hook="${hook}"]`);
-}
-
-function elementsByDataHook(hook) {
-  return $$(`[data-hook="${hook}"]`);
-}
-
-function waitForElement(element, timeout = 1000, optionalMessage) {
-  return browser.wait(function () {
-    return element.isPresent().then(function (isPresent) {
-      if (isPresent) {
-        return element.isDisplayed();
-      }
-      else {
-        return false;
-      }
-    });
-  }, timeout, optionalMessage);
-}
-
 describe('Movies App', () => {
 
   beforeEach(() => {
     browser.get('/');
-    waitForElement(elementByDataHook('movie-list-title'));
   });
 
   it('should have a title', () => {
     expect(browser.getTitle()).toEqual('Movies App');
-  });
-
-  it('should make a happy flow', () => {
-    const secondMovie = elementsByDataHook('movie').get(1);
-    secondMovie.$('[data-hook="title"]')
-      .getText()
-      .then(movieTitleInTheMainPage => {
-        secondMovie.click();
-        expect(elementByDataHook('title').getText()).toContain(movieTitleInTheMainPage);
-      });
   });
 });
--- src/app/movies/components/movie-list/movie-item.driver.js
+++
@@ -1,9 +0,0 @@
-export class MovieItemDriver {
-  constructor(component) {
-    this.component = component;
-  }
-
-  get = {
-    title: () => this.component.find('[data-hook="title"]')
-  };
-}
